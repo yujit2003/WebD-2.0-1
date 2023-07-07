c4463fc6513b0d80989140868191ed3d6f492548
yujit2003 <yujityadav99@gmail.com>
2023-07-08 00:36:43 +0530

    home hero page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
+import Header from "./components/Header.jsx";
+import Home from "./components/Home.jsx";
+import Footer from "./components/Footer.jsx";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <Header /> */}
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
