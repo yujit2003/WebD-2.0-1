e1238879a08ed07de8545c680f5e13d847b3c4d4
Ashwani <ashwanisingh46572@gmail.com>
2023-07-12 20:13:40 +0530

    updated tablinks

---
+++ src/data.jsx
@@ -0,0 +1,54 @@
+// import home from "./assets/images/emr_logo.png"
+// import testBG from "./assets/images/giphy.gif"
+// import Home1 from "./pages/Home"
+// import Blog from "./components/BlogCard/BlogPage"
+
+//------- background of tab links ------
+import Tech_fest from "./assets/images/Tech_fest.png"
+import gallery from "./assets/images/gallery.png"
+import projects from  "./assets/images/projects.png"
+import workshop from "./assets/images/workshop.png"
+import blog from "./assets/images/blog.png"
+import team from "./assets/images/team.png"
+
+
+export const tabData = [
+  {
+    label: 'TECH FEST',
+     path: '/',
+     backgroundImage: Tech_fest 
+  },
+  {
+    label: 'Workshops',
+     path: '/',
+     backgroundImage: workshop
+    },
+   {
+      label: 'PROJECTS',
+       path: '/',
+       backgroundImage: projects
+    },
+    {
+      label: 'Blog',
+       path: '/blog',
+       backgroundImage: blog
+    },
+  
+    {
+      label: 'GALLERY',
+       path: '/',
+       backgroundImage: gallery
+    },
+    {
+      label: 'TEAM',
+       path: '/',
+       backgroundImage: team
+    },
+    {
+      label: 'SOON..',
+       path: '/',
+       backgroundImage: blog
+    },
+    
+ 
+];
