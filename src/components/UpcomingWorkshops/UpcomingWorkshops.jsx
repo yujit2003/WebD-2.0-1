91c3b9f1426080b2e2f32581815cd1f89fc56a02
Shourya Tyagi <shouryatyagi042@gmail.com>
2023-07-17 23:29:00 +0530

    Added button & text

--- src/components/UpcomingWorkshops/UpcomingWorkshops.jsx
+++ src/components/UpcomingWorkshops/UpcomingWorkshops.jsx
@@ -2,8 +2,9 @@ import "../../assets/css/UpcomingWorkshop.css";
 import React from "react";
 import { useState } from "react";
 import Slider from "react-slick";
-import pic1 from "../../assets/images/WorkshopSlider.png";
+import pic1 from "../../assets/images/home_bg.gif";
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import ReactTyped from "react-typed";
 
 const images = [pic1, pic1, pic1, pic1, pic1];
 
@@ -29,8 +30,8 @@ const UpcomingWorkshops = () => {
   const settings = {
     infinite: true,
     lazyLoad: true,
-    speed: 150,
-    slidesToShow: 3,
+    speed: 50,
+    slidesToShow: 1,
     centerMode: true,
     centerPadding: 0,
     nextArrow: <NextArrow />,
@@ -39,14 +40,23 @@ const UpcomingWorkshops = () => {
   };
 
   return (
-    <div className="App">
-      <Slider {...settings}>
-        {images.map((img, idx) => (
-          <div className={idx === imgIndex ? "slide activeSlide" : "slide"}>
-            <img src={img} alt={idx} />
-          </div>
-        ))}
-      </Slider>
+    <div className="upcoming_workshop_block">
+      <p className="upcoming_workshop_text">
+        Upcoming <ReactTyped strings={["Workshops"]} typeSpeed={100} loop />
+      </p>
+
+      <div className="slider_block">
+        <Slider {...settings}>
+          {images.map((img, idx) => (
+            <div className={idx === imgIndex ? "slide activeSlide" : "slide"}>
+              <img src={img} alt={idx} />
+            </div>
+          ))}
+        </Slider>
+      </div>
+      <button class="bg-transparent hover:bg-green-500 text-green-500 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded">
+        Apply Now
+      </button>
     </div>
   );
 };
