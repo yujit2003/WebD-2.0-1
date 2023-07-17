d9c21c21d9418f0ad04ac0503ca6b5f4efed86a2
Shourya Tyagi <shouryatyagi042@gmail.com>
2023-07-17 19:57:01 +0530

    Added the Carousal

--- src/components/UpcomingWorkshops/UpcomingWorkshops.jsx
+++ src/components/UpcomingWorkshops/UpcomingWorkshops.jsx
@@ -1,7 +1,54 @@
-import React from 'react';
+import "../../assets/css/UpcomingWorkshop.css";
+import React from "react";
+import { useState } from "react";
+import Slider from "react-slick";
+import pic1 from "../../assets/images/WorkshopSlider.png";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+
+const images = [pic1, pic1, pic1, pic1, pic1];
 
 const UpcomingWorkshops = () => {
-  return <h1>Hello, JSX!</h1>;
+  const NextArrow = ({ onClick }) => {
+    return (
+      <div className="arrow next" onClick={onClick}>
+        <FaArrowRight />
+      </div>
+    );
+  };
+
+  const PrevArrow = ({ onClick }) => {
+    return (
+      <div className="arrow prev" onClick={onClick}>
+        <FaArrowLeft />
+      </div>
+    );
+  };
+
+  const [imgIndex, setImgIndex] = useState(0);
+
+  const settings = {
+    infinite: true,
+    lazyLoad: true,
+    speed: 150,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: 0,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    beforeChange: (_current, next) => setImgIndex(next),
+  };
+
+  return (
+    <div className="App">
+      <Slider {...settings}>
+        {images.map((img, idx) => (
+          <div className={idx === imgIndex ? "slide activeSlide" : "slide"}>
+            <img src={img} alt={idx} />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 };
 
 export default UpcomingWorkshops;
