4c1168b4e0d6fb6df9a664722abf0267955eac5b
Tapasvi48 <Tapasviarora2002@gmail.com>
2023-07-26 11:04:28 +0530

    " changed file location"

--- src/components/Team/Card.jsx
+++ src/components/Team/Card.jsx
@@ -7,36 +7,38 @@ import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
 import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons"
-import img1 from "/Users/tapasviarora/Desktop/WebD-2.0/src/components/Team/Profile Photo (File responses)/priyanshu - Priyanshu Narwaria.jpg";
-import img2 from "/Users/tapasviarora/Desktop/WebD-2.0/src/components/Team/Profile Photo (File responses)/01F024EA-4C85-4C8A-AA2C-81A72E1159B5 - Priyanshi.jpeg";
-import img3 from "/Users/tapasviarora/Desktop/WebD-2.0/src/components/Team/Profile Photo (File responses)/InShot_20230516_220552985 - 143_SUNIL RATHORE.jpg"
-import img4 from "/Users/tapasviarora/Desktop/WebD-2.0/src/components/Team/Profile Photo (File responses)/IMG_9481 - KRITIKA.jpeg"
-import img5 from "/Users/tapasviarora/Desktop/WebD-2.0/src/components/Team/Profile Photo (File responses)/20220922_134905 - Nicky.jpg"
-import img6 from "/Users/tapasviarora/Desktop/WebD-2.0/src/components/Team/Profile Photo (File responses)/Arpit_Image_square - ARPIT BHATNAGAR.jpg"
-import img7 from "/Users/tapasviarora/Desktop/WebD-2.0/src/components/Team/Profile Photo (File responses)/PXL_20230202_044345959 - 5159_Ritesh_ecc9.jpg"
-import img8 from "/Users/tapasviarora/Desktop/WebD-2.0/src/components/Team/Profile Photo (File responses)/Deepan - Deepan Adak.jpg"
+import img1 from "../../assets/images/ProfileImage/priyanshu - Priyanshu Narwaria.jpg";
+import img2 from "../../assets/images/ProfileImage/01F024EA-4C85-4C8A-AA2C-81A72E1159B5 - Priyanshi.jpeg";
+import img3 from "../../assets/images/ProfileImage/InShot_20230516_220552985 - 143_SUNIL RATHORE.jpg"
+import img4 from "../../assets/images/ProfileImage/IMG_9481 - KRITIKA.jpeg"
+import img5 from "../../assets/images/ProfileImage/20220922_134905 - Nicky.jpg"
+import img6 from "../../assets/images/ProfileImage/Arpit_Image_square - ARPIT BHATNAGAR.jpg"
+import img7 from "../../assets/images/ProfileImage/PXL_20230202_044345959 - 5159_Ritesh_ecc9.jpg"
+import img8 from "../../assets/images/ProfileImage/Deepan - Deepan Adak.jpg"
 const cardData=[
 {
 "name":"Priyanshu Narwaria",
 "insta":"https://www.instagram.com/napshot_priyanshu/",
 "linkedin":"https://www.linkedin.com/in/priyanshu-narwaria-153874201/",
 "github":"https://github.com/napshotpriyanshu",
-"image":img1
+"image":img1,
+"role":"Joint Secretary",
 },
 {
   "name":"Priyanshi ",
   "insta":"https://instagram.com/priyanshirao_18?igshid=MjEwN2IyYWYwYw==",
   "linkedin":"https://www.linkedin.com/in/priyanshi-rao13/",
   "github":"https://github.com/priyanshiRao",
-  "image":img2
-
+  "image":img2,
+  "role":"Vice President",
   },
 {
     "name":"Sunil Singh Rathore ",
     "insta":"https://www.instagram.com/rathore._.15/",
     "linkedin":"https://www.linkedin.com/in/sunil-singh-rathore-4a1831202",
     "github":"https://github.com/itsrathore15",
-    "image":img3
+    "image":img3,
+    "role":"Secretary",
 
 
 },
@@ -45,7 +47,8 @@ const cardData=[
   "insta":"",
   "linkedin":"https://www.linkedin.com/in/kritika-mittal-3a6182238/",
   "github":"https://github.com/m-kritika",
-  "image":img4
+  "image":img4,
+  "role":"Member",
 
   },
 {
@@ -53,14 +56,16 @@ const cardData=[
     "insta":"https://instagram.com/n.i.c.k.y_singh?igshid=MzNlNGNkZWQ4Mg==",
     "linkedin":"https://www.linkedin.com/in/nicky-singh-a666881b6",
     "github":"https://github.com/NickySingh44",
-"image":img5
+"image":img5,
+"role":"Member",
 },
 {
   "name":"Arpit Bhatnagar",
   "insta":"https://www.instagram.com/bhatnagar.arpit2021/",
   "linkedin":"linkedin.com/in/arpit-bhatnagar-529213221/",
   "github":"github.com/Sheldon38",
-  "image":img6
+  "image":img6,
+  "role":"Member",
 
   },
   {
@@ -69,6 +74,7 @@ const cardData=[
     "linkedin":"https://www.linkedin.com/in/ritesh-madishetty-a36844202",
     "github":"",
     "image":img7,
+    "role":"Member",
 
     },
 {
@@ -77,6 +83,7 @@ const cardData=[
       "linkedin":"https://www.linkedin.com/in/deepan-adak/",
       "github":"https://github.com/mr-prometheus",
       "image":img8,
+      "role":"President",
 
 }      
 
@@ -130,7 +137,7 @@ const [visit3,setVisit3]=useState(false);
 </div>
 <div className='details'>
 <h3>{item.name}</h3>   
-<p>Secert Agent</p>
+<p>{item.role}</p>
 <div className='socialicons'>
 <a href={item.linkedin}
 target="_blank" rel="noopener noreferrer"
