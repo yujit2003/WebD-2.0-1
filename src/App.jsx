3ccf9393c81ea534665053d1ea200105fec78134
yujit2003 <yujityadav99@gmail.com>
2023-08-11 03:31:13 +0530

    workshop component added

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import {    Navbar, StarsCanvas ,
+  // TabLinks, Hero, ,BlogPage,Team,SponsorPage
+} from "./components";
+
+import Home from './pages/Home';
+import GalleryComp from './pages/GalleryPage';
+import TeamComp from './pages/TeamPage';
+// import Blog from './pages/Blog';
+import Workshop from './pages/WorkshopPage';
+import WorkshopDetail from './components/WorkshopPage/WorkshopDetail'
+import ContactUs from './components/ContactUs/Contact'
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+
+<Navbar/>
+<StarsCanvas /> 
+
+      <Routes>
+        <Route path="/Roger28-51-emr/" element={<Home />} />
+
+        <Route path="/Roger28-51-emr/team" element={<TeamComp />} />
+        {/* <Route path="/Roger28-51-emr/sponser" element={<SponsorPage />} /> */}
+   
+
+        <Route path="/Roger28-51-emr/gallery" element={<GalleryComp/>} />
+        <Route path="/Roger28-51-emr/workshop" element={<Workshop/>} />
+        <Route path="/Roger28-51-emr/workshop/detail" element={<WorkshopDetail/>} />
+      </Routes>
+
+    </BrowserRouter>
+
+
+
+  );
+}
+
+export default App;
+
---
+++ src/components/WorkshopPage/WorkshopDetails.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Bullet from './Bullet.jsx'
+import './workshop.css'
+
+const Workshops = ({ws}) => {
+  return (
+  <div className='workshop_container'>
+   <div className="workshop_title">
+    <h1 data-text="WORKSHOPS">WORKSHOPS</h1>
+    </div>
+   
+      <div className='workshop_cards'>
+                {
+                  ws.map( (workshop) => {
+                        return <Bullet key={workshop.id} {...workshop}/>
+                    } )
+                }
+            </div>
+            </div>
+  );
+};
+
+export default Workshops;
